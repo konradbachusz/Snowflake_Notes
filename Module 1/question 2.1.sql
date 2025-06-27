bd72ea207e997feed7b22a4d8dd13618fedbd52d
Konrad Bachusz <konrad.bachusz@credera.co.uk>
2025-06-27 16:56:36 +0100

    Refactor SQL scripts: remove outdated queries and add new warehouse and table creation scripts

---
+++ Module 1/question 2.1.sql
@@ -0,0 +1,6 @@
+-- Create a warehouse named "warehouse_one"
+CREATE WAREHOUSE warehouse_one;
+
+-- Show metadata about the warehouse
+SHOW WAREHOUSES;
+
---
+++ Module 1/question 2.2.sql
@@ -0,0 +1,12 @@
+
+
+-- Create a new warehouse named "warehouse_two"
+CREATE WAREHOUSE warehouse_two;
+
+-- Switch to using "warehouse_two"
+USE WAREHOUSE warehouse_two;
+
+-- Show metadata about the warehouses
+SHOW WAREHOUSES;
+
+
